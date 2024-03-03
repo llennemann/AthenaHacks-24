8a942dccebcc62ce1bba7acaec0d63ebb8dd985b
Shrishti <SPECTRE@MacBook-Pro-7.local>
2024-03-02 20:22:50 -0800

    backend work

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,18 @@
 import time
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+total_time = 0 #in days
 
 @app.route('/')
-def get_current_time():
-    return {'time': time.time()}
+def home():
+    return render_template("landingPage.html")
+    
+@app.route('/submit_time' , methods=['POST'])
+def submit_time():
+     years = request.form['number']
+     total_time = int(years) * 365
+     return render_template("submit_time.html", total_time = total_time)
+
+if __name__ == '__main__':
+    app.run()
