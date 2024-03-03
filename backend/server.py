2585ca29779c8d8868e6957aad82c96c664d5bef
Shrishti <SPECTRE@MacBook-Pro-7.local>
2024-03-03 00:22:08 -0800

    Buy table done

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,8 @@
 import time
 from flask import Flask, request, render_template
+from data_generator import all_data
+
+curr_day = 0
 
 app = Flask(__name__)
 total_time = 0 #in days
@@ -12,14 +15,27 @@ def home():
 def tryMe():
     return render_template("tryMe.html")
     
+def getTableData(currentDay):
+    industries, stocks = all_data()
+    table = {} # result dictionary
+    for key in stocks.keys():
+        table[key] = [industries.get(key), stocks.get(key)[currentDay]]
+
+    return table
+    
 @app.route('/buy' , methods=['POST'])
 def buy():
      years = request.form['number']
      total_time = int(years) * 365
-     return render_template("buy.html", total_time = total_time)
+     return render_template("buy.html", total_time = total_time, stock_data=getTableData(curr_day))
+
 @app.route('/portfolio')
 def portfolio():
     return render_template("portfolio.html")
+
+@app.route('/exchange')
+def exchange():
+    return render_template("exchange.html")
 
 if __name__ == '__main__':
     app.run()
