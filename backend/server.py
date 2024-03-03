6a335375a7f36e66544ed95f49a45d3da538bd36
Lucy Lennemann <llennema@ucsd.edu>
2024-03-02 22:47:26 -0800

    Routing for landing pg

--- backend/server.py
+++ backend/server.py
@@ -7,6 +7,20 @@ total_time = 0 #in days
 @app.route('/')
 def home():
     return render_template("landingPage.html")
+
+@app.route('/tryMe')
+def tryMe():
+    return render_template("tryMe.html")
+
+#def landingPgRouting():
+
+# def contact():
+#     if request.form['submit_button'] == 'try me!':
+#         return render_template('tryMe.html')
+#     elif request.form['submit_button'] == 'Do Something Else':
+#         pass # do something else
+#     else:
+#         pass # unknown
     
 @app.route('/submit_time' , methods=['POST'])
 def submit_time():
